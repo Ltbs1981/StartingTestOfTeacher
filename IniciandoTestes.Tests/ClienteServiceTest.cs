9cc628bd232aee6d452baf88754d5c48daba7363
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-11-22 20:20:55 -0300

    projeto base pronto inicial

---
+++ IniciandoTestes.Tests/ClienteServiceTest.cs
@@ -0,0 +1,73 @@
+﻿using Bogus;
+using IniciandoTestes.Contratos;
+using IniciandoTestes.Entidades;
+using IniciandoTestes.Servicos;
+using Moq;
+using System;
+using Xunit;
+
+namespace IniciandoTestes.Tests
+{
+    public class ClienteServiceTest
+    {
+        [Fact]
+        public void AdicionarCLiente_DeveAdicionarComSucesso_QuandoClienteValido()
+        {
+            //Arrange
+            Mock<IClienteRepository> clienteRepositoryMock = new Mock<IClienteRepository>();
+            clienteRepositoryMock.Setup(x => x.GetCliente(It.IsAny<Guid>())).Returns(new Cliente());
+            ClienteService sut = new ClienteService(clienteRepositoryMock.Object);
+            Faker faker = new Faker();
+            Cliente cliente = new Cliente()
+            {
+                Id = Guid.NewGuid(),
+                Nome = faker.Name.FullName(),
+                Nascimento = new DateTime(1900, 12, 12)
+            };
+
+            //Act
+
+            sut.AddClliente(cliente);
+
+            //Assert
+
+            clienteRepositoryMock.Verify(x => x.GetCliente(It.IsAny<Guid>()), Times.Once());
+            clienteRepositoryMock.Verify(x => x.AddCliente(cliente), Times.Once());
+
+        }
+
+        [Fact]
+        public void TesteEx()
+        {
+            Mock<IClienteRepository> mock = new Mock<IClienteRepository>();
+            ClienteService clienteService = new ClienteService(mock.Object);
+
+            var result = clienteService.ExemploAtrasadinhoQueNaoAvisaEDepoisEncheOSaco();
+            var resultadoEsperado = "Responda a mensagem na proxima vez";
+            Assert.NotNull(result);
+            Assert.Equal(resultadoEsperado, result);
+
+        }
+
+        [Fact]
+        public void AddCliente_DeveQuebrar_QuandoClienteJaExiste()
+        {
+            //Arrange
+            Faker faker = new Faker();
+            Cliente cliente = new Cliente()
+            {
+                Nome = faker.Name.FullName(),
+                Nascimento = new System.DateTime(1900, 12, 12),
+                Id = Guid.NewGuid(),
+            };
+
+            Mock<IClienteRepository> clienteRepositoryMock = new Mock<IClienteRepository>();
+            clienteRepositoryMock.Setup(x => x.GetCliente(It.IsAny<Guid>())).Returns(cliente);
+
+            ClienteService sut = new ClienteService(clienteRepositoryMock.Object);
+
+            //Act - Assert   // x => x.  -- () => 
+            Assert.Throws<Exception>(() => sut.AddClliente(cliente));
+        }
+    }
+}
---
+++ IniciandoTestes.Tests/FuncionarioServiceTest.cs
@@ -0,0 +1,69 @@
+﻿using Bogus;
+using System.Collections.Generic;
+using System;
+using Xunit;
+using IniciandoTestes.Entidades;
+using IniciandoTestes.Servicos;
+
+namespace IniciandoTestes.Tests
+{
+    public class FuncionarioServiceTest
+    {
+        private readonly Faker _faker;
+
+        public FuncionarioServiceTest()
+        {
+            _faker = new Faker();
+        }
+
+
+        [Theory]
+        [MemberData(nameof(GetFuncionariosData))]
+        public void AdicionarFuncionario_DeveConcluir_QuandoDadosValidos(Funcionario funcionario, int n1, string exemplo)
+        {
+            //Arrange
+            FuncionarioService sut = new FuncionarioService();
+
+            //Act - Assert
+            sut.AdicionarFuncionario(funcionario);
+        }
+
+        public static IEnumerable<object[]> GetFuncionariosData()
+        {
+            var faker = new Faker();
+
+            yield return new object[]
+            {
+                new Funcionario()
+                {
+                    Nome = faker.Name.FullName(),
+                    Nascimento = faker.Date.Between(DateTime.Now.AddDays(-21), DateTime.Now.AddDays(-50)),
+                    Senioridade = Senioridade.Junior,
+                    Salario = faker.Random.Double(3200,5500)
+                },
+                2,
+                "Sucesso"
+            };
+
+            yield return new object[] { new Funcionario()
+                {
+                    Nome = faker.Name.FullName(),
+                    Nascimento = faker.Date.Between(DateTime.Now.AddDays(-21), DateTime.Now.AddDays(-50)),
+                    Senioridade = Senioridade.Pleno,
+                    Salario = faker.Random.Double(5500,8000)
+                },
+                5,
+                "Falha"
+            };
+
+            yield return new object[] { new Funcionario()
+                {
+                    Nome = faker.Name.FullName(),
+                    Nascimento = faker.Date.Between(DateTime.Now.AddDays(-21), DateTime.Now.AddDays(-50)),
+                    Senioridade = Senioridade.Senior,
+                    Salario = faker.Random.Double(8000,20000)
+                }, 10, "Uhuuul"
+            };
+        }
+    }
+}
